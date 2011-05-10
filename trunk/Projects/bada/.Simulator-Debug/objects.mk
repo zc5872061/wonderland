128908efbd00d1c77cff0195c8a1cbc3e921c6d4
Artur.S.Mazurek@gmail.com <Artur.S.Mazurek@gmail.com@b841bdc2-4e26-aa7d-a250-558d36fd1405>
2011-05-10 21:25:37 +0000

    Also deleted the .simulator-debug for wonderland

--- trunk/Projects/bada/.Simulator-Debug/objects.mk
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -losp_rt0 -lFreetypeBada -lFGraphicsOpengl -lFGraphicsEgl -llibc-newlib -lFOsp
